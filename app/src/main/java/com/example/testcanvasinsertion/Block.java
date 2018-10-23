50e16433e9ec665d23e06ecdf1fb54082c91c1b3
aki0207 <forever.look.aki@docome.ne.jp>
2018-10-23 16:50:30 +0900

    HandlerをMyviewクラスからMainActivityにうつした。任意の時間ごとに落下させられるようになったし、落下とボタンでの移動処理はそれぞれ機能している。ブロックの落下は壁にあたったときに止まるが、落下がとまったあとも左右に動かせられるのでそこを修正する

--- app/src/main/java/com/example/testcanvasinsertion/Block.java
+++ app/src/main/java/com/example/testcanvasinsertion/Block.java
@@ -36,10 +36,12 @@ public class Block {
         block.block[2][1] = 2;
         block.block[2][2] = 2;
 
+        //落ち始めの開始位置
+        //field_xの開始位置2529から8回移動.1マスの移動で 51動くから252 + 51* 8 = 660
         block.block_x = 660;
-        block.block_y = 251;
-        block.block_width = 719;
-        block.block_height = 301;
+        block.block_y = 250;
+        block.block_width = 710;
+        block.block_height = 300;
 
         block.block_x_evacuation = 660;
         block.block_width_evacuation = 719;
@@ -70,10 +72,12 @@ public class Block {
 
     public void leftMove(Block block) {
 
+        //-102は強引過ぎて笑ってる。MyViewクラスでのblock_yとheigth、Blockクラスでのそれぞれに102の差異がある
+        //MyViewで描画位置変えるたびにBlockに値を反映していればこの102はせんでいいばい
         this.block_x = block.block_x - 50;
-        this.block_y = 251;
+        this.block_y = block.block_y - 102;
         this.block_width = block.block_width - 50;
-        this.block_height = 301;
+        this.block_height = block.block_height - 102;
 
 
         this.block_point_left_top_y = block_point_left_top_y - 1;
@@ -87,15 +91,33 @@ public class Block {
     public void rightMove(Block block) {
 
         this.block_x = block.block_x + 50;
-        this.block_y = 251;
+        this.block_y = block.block_y - 102;
         this.block_width = block.block_width + 50;
-        this.block_height = 301;
+        this.block_height = block.block_height - 102;
 
 
         this.block_point_left_top_y = block_point_left_top_y + 1;
         this.block_point_right_top_y = block_point_right_top_y + 1;
         this.block_point_left_bottom_y = block_point_left_bottom_y + 1;
         this.block_point_right_bottom_y = block_point_right_bottom_y + 1;
+
+
+    }
+
+    public void downMove(Block block) {
+
+        //描画座標修正
+        this.block_x = block.block_x;
+        this.block_y = this.block_y - 51;
+        this.block_width = block.block_width;
+        this.block_height = this.block_height - 51;
+
+
+        //配列内におけるブロックの現在位置修正
+        this.block_point_left_top_x = block_point_left_top_x + 1;
+        this.block_point_right_top_x = block_point_right_top_x + 1;
+        this.block_point_left_bottom_x = block_point_left_bottom_x + 1;
+        this.block_point_right_bottom_x = block_point_right_bottom_x + 1;
 
 
     }
@@ -130,17 +152,32 @@ public class Block {
 
     }
 
-    //ブロックが二次元配列のどの部分に存在するのか座標を返す
-    public  Block getBlockCoordinate(Block block) {
+    //下方向に壁、ブロックがないか調べる
+    public boolean isDownMoveJudge(int[][] field, int block_point_left_bottom_x, int block_point_left_bottom_y, int block_point_right_bottom_x, int block_point_right_bottom_y) {
 
-         block.block_point_left_top_x = block_point_left_top_x;
-         block.block_point_left_top_y = block_point_left_top_y;
-         block.block_point_right_top_x = block_point_right_top_x;
-         block.block_point_right_top_y = block_point_right_top_y;
-         block.block_point_left_bottom_x = block_point_left_bottom_x;
-         block.block_point_left_bottom_y = block_point_left_bottom_y;
-         block.block_point_right_bottom_x = block_point_right_bottom_x;
-         block.block_point_right_bottom_y = block_point_right_bottom_y;
+        boolean move_flg = true;
+
+        if (field[block_point_left_bottom_x + 1][block_point_left_bottom_y] == 1 || field[block_point_right_bottom_x + 1][block_point_right_bottom_y] == 1) {
+
+            move_flg = false;
+
+        }
+
+        return move_flg;
+
+    }
+
+    //ブロックが二次元配列のどの部分に存在するのか座標を返す
+    public Block getBlockCoordinate(Block block) {
+
+        block.block_point_left_top_x = block_point_left_top_x;
+        block.block_point_left_top_y = block_point_left_top_y;
+        block.block_point_right_top_x = block_point_right_top_x;
+        block.block_point_right_top_y = block_point_right_top_y;
+        block.block_point_left_bottom_x = block_point_left_bottom_x;
+        block.block_point_left_bottom_y = block_point_left_bottom_y;
+        block.block_point_right_bottom_x = block_point_right_bottom_x;
+        block.block_point_right_bottom_y = block_point_right_bottom_y;
 
         return block;
     }
